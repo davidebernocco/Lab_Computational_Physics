07babe9f8e4e017be1e1783cad6e4fc740c0e5ef
Davide Bernocco <davide.plantageneto@gmail.com>
2023-10-24 18:31:04 +0200

    Add files via upload

--- week_3/Esercizi_setimana3.py
+++ week_3/Esercizi_setimana3.py
@@ -9,11 +9,12 @@ import numpy as np
 from scipy.optimize import curve_fit
 from numba import jit, njit, int32, float64
 import math
+import time
 
 
 #-- ES 1 --
 #---------- Random numbers with non-uniform distribution: Inverse Trasformation Method (ITM)
-
+"""
 num_rand = 10**6
 data = np.random.rand(num_rand)
 a=3
@@ -65,5 +66,80 @@ plt.title('Histogram with Linear Fit')
 plt.legend()
 plt.grid(True)
 plt.show()
+"""
+
+#-- ES 2 --
+#---------- Random numbers with non-uniform distribution: comparison between different algorithms
+
+#2.1 with ITM
+
+num_rand = 10**6
+
+start_time1 = time.time()
+data = np.random.rand(num_rand)
+exp_data = np.sin(math.pi*(2*data - 1))
+
+IQR = np.percentile(data, 75) - np.percentile(data, 25)
+bins = int((max(data) - min(data)) / (2 * IQR * len(data)**(-1/3)))
+
+hist, bins = np.histogram(exp_data, bins, density=False)
+bin_centers = (bins[:-1] + bins[1:]) / 2
+bin_widths = np.diff(bins)
+density = hist / sum(hist)
+
+plt.bar(bins[:-1], density, width=bin_widths, alpha=0.5, color='b', label=r'$ p(x) = \frac{1}{\pi} \frac{1}{\sqrt{1 - x^{2}}} $')
+plt.xlabel('Counts')
+plt.ylabel('Probability Density')
+plt.title('Normalized Histogram - U-shaped distributed variable (ITM)')
+plt.legend()
+plt.grid()
+plt.show()
+
+end_time1 = time.time()
+elapsed_time1 = end_time1 - start_time1
+print(f"Elapsed time1: {elapsed_time1:.4f} seconds")
+
+
+#2.2 with an algorithm that uses only elementary operations
+start_time2 = time.time()
+varU = np.random.rand(num_rand)
+varV = np.random.rand(num_rand)
+
+@jit
+def var_x(U,V):
+    x = []
+    for i in range(num_rand):
+        if U[i]**2 + V[i]**2 <= 1 :
+            x.append((U[i]**2 - V[i]**2)/(U[i]**2 + V[i]**2))
+    return x
+
+IQR2 = np.percentile(var_x(varU, varV), 75) - np.percentile(var_x(varU, varV), 25)
+bins2 = int((max(var_x(varU, varV)) - min(var_x(varU, varV))) / (2 * IQR2 * len(var_x(varU, varV))**(-1/3)))
+
+hist2, bins2 = np.histogram(var_x(varU, varV), bins, density=False)
+bin_centers2 = (bins2[:-1] + bins2[1:]) / 2
+bin_widths2 = np.diff(bins2)
+density2 = hist2 / sum(hist2)
+
+plt.bar(bins2[:-1], density2, width=bin_widths2, alpha=0.5, color='b', label=r'$ p(x) = \frac{1}{\pi} \frac{1}{\sqrt{1 - x^{2}}} $')
+plt.xlabel('Counts')
+plt.ylabel('Probability Density')
+plt.title('Normalized Histogram - U-shaped distributed variable')
+plt.legend()
+plt.grid()
+plt.show()
+
+end_time2 = time.time()
+elapsed_time2 = end_time2 - start_time2
+print(f"Elapsed time2: {elapsed_time2:.4f} seconds")
+
+
+
+
+
+
+
+
+
 
 
