5013aae3692d47a6fbf3ae73e85f4a85eec6652d
Davide Bernocco <davide.plantageneto@gmail.com>
2023-12-11 14:52:29 +0100

    Add files via upload

--- week_4/Funz4.py
+++ week_4/Funz4.py
@@ -1,5 +1,5 @@
 """
-Library of self-made functions needed for the codes implemented for the exercises of the 3rd week
+Library of self-made functions needed for the codes implemented for the exercises of the 4th week
 
 @author: david
 """
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from scipy.stats import norm
 import math
+from matplotlib.animation import PillowWriter
 
 
 
@@ -241,4 +242,122 @@ def RW1D_average_random_l(N_w, N, x0, Pl):
     average_x2 = cumul_x2 / N_w
 
     return position, square_pos, average_x, average_x2, average_x2 - average_x**2 , P_N
+
+
+
+# -----------------------------------------------------------------------------
+# ----------- Functions for the 2d_RW part:
+
+
+@njit
+def RW2D_average(N_w, N, x0, y0, Pl, Pr, Pd, Theta):  # 2D analogous of the "RW1D_average" function in Funz4
+    
+    position_x = np.full((N_w, N + 1), x0, dtype=np.float32)
+    position_y = np.full((N_w, N + 1), x0, dtype=np.float32)
+    cumul_x = np.zeros(N, dtype=np.float32)
+    cumul_y = np.zeros(N, dtype=np.float32)
+    cumul_x2 = np.zeros(N, dtype=np.float32)
+    cumul_y2 = np.zeros(N, dtype=np.float32)
+    P_N = np.empty(N_w, dtype=np.float64)
+    
+    if not Theta:
+        
+        for j in range(N_w):
+            
+            xi = x0
+            yi = y0
+            a = np.random.uniform(0, 1, N)
+            #lst_l = np.random.uniform(0, 1, N)   # If we want to work on a lattice of random spacing l for each step
+            lst_l = np.asarray([1 for _ in range(N)], dtype=np.int32)   # If we want to work on a descrete lattice of spacing l = 1
+            
+            for i in range(N):
+                
+                if a[i] <= Pl:
+                    xi -= lst_l[i] 
+                    position_x[j, i + 1] = xi
+                    position_y[j, i + 1] = yi
+                    cumul_x[i] += xi
+                    cumul_x2[i] += xi**2
+                elif a[i] > Pl and a[i] <= (Pl + Pr):
+                    xi += lst_l[i]
+                    position_x[j, i + 1] = xi
+                    position_y[j, i + 1] = yi
+                    cumul_x[i] += xi
+                    cumul_x2[i] += xi**2
+                elif a[i] > (Pl + Pr) and a[i] <= (Pl + Pr + Pd):
+                    yi -= lst_l[i] 
+                    position_y[j, i + 1] = yi
+                    position_x[j, i + 1] = xi
+                    cumul_y[i] += yi
+                    cumul_y2[i] += yi**2
+                else:
+                    yi += lst_l[i]  
+                    position_y[j, i + 1] = yi
+                    position_x[j, i + 1] = xi
+                    cumul_y[i] += yi
+                    cumul_y2[i] += yi**2  
+        
+            P_N[j] = math.sqrt(xi**2 + yi**2)   
+                     
+    else:  # For 2D random unit steps (on the unitary circle)
+        
+        for j in range(N_w):
+            
+            xi = x0
+            yi = y0
+            th = np.random.uniform(0, 2*math.pi, N)
+            
+            for i in range(N):
+                xi += math.cos(th[i])
+                yi += math.sin(th[i])
+                position_x[j, i + 1] = xi
+                position_y[j, i + 1] = yi
+                cumul_x[i] += xi
+                cumul_x2[i] += xi**2
+                cumul_y[i] += yi
+                cumul_y2[i] += yi**2  
+            
+            P_N[j] = math.sqrt(xi**2 + yi**2) 
+            
+        
+    average_x = cumul_x / N_w
+    average_y = cumul_y / N_w
+    average_x2 = cumul_x2 / N_w
+    average_y2 = cumul_y2 / N_w
+
+    return position_x, position_y, average_x2 + average_y2 - average_x**2 -  average_y**2 , P_N
+
+
+
+
+def RW_2D_plot(Opera, plot_name):  #Plotting the gif of all the walkers justapposed
+
+    fig = plt.figure()
+    
+    l = [plt.plot([], [])[0] for _ in range(len(Opera[0]))]
+    xlist = [[] for _ in range(len(Opera[0]))]
+    ylist = [[] for _ in range(len(Opera[0]))]
+    
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('2D RW')
+    plt.xlim(np.min(Opera[0]) - 1, np.max(Opera[0]) + 1)
+    plt.ylim(np.min(Opera[1]) - 1, np.max(Opera[1]) + 1)
+    plt.grid(True) 
+    plt.gca().set_aspect('equal', adjustable='box')
+    
+    metadata = dict(title='Movie', artist='codinglikened')
+    writer = PillowWriter(fps=1, metadata= metadata)
+    
+    with writer.saving(fig, plot_name, 100):
+        for k in range(len(Opera[2])+1):
+            for i in range(len(Opera[0])):
+                xlist[i].append(Opera[0][i][k])
+                ylist[i].append(Opera[1][i][k])
+              
+                l[i].set_data(xlist[i], ylist[i])
+               
+            writer.grab_frame()
+            
+    return
 
