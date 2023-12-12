2ec6e89256d2029f29a6b7110b71ddd2a93064ec
Davide Bernocco <davide.plantageneto@gmail.com>
2023-12-12 02:00:05 +0100

    Add files via upload

--- week_4/gif_2DRW.py
+++ week_4/gif_2DRW.py
@@ -10,32 +10,51 @@ from Funz4 import RW2D_average, RW_2D_plot
 
 # -----------------------------
 # ---- A) On a square lattice
-
-VonKarajan = RW2D_average(10000, 64, 0, 0, 0.25, 0.25, 0.25, False)
+"""
+VonKarajan = RW2D_average(5000, 32, 0, 0, 0.25, 0.25, 0.25, False)
 
 RW_2D_plot(VonKarajan, '2D_RW.gif')
 plt.close('all')
 
 
+# Create an example array with real numbers
+original_array = VonKarajan[3]
+
+# Use NumPy's unique function to get unique entries and their indices
+unique_entries, indices = np.unique(original_array, return_inverse=True)
+
+# Create a dictionary to store separated arrays
+separated_arrays = {}
+
+# Iterate through unique entries and populate the dictionary
+for entry in unique_entries:
+    separated_arrays[entry] = original_array[np.isclose(original_array, entry)]
+
+# Print the separated arrays
+bins = np.asarray([], dtype=np.float32)
+columns = np.asarray([], dtype=np.int32)
+for entry, array in separated_arrays.items():
+    bins = np.append(bins, entry)
+    columns = np.append(columns, len(array))
+#print(bins, columns)
+
+
 # Normalized Histogram - distribution of position at the end of the walkers
-IQR = np.percentile(VonKarajan[3], 75) - np.percentile(VonKarajan[3], 25)
-nbins = int((max(VonKarajan[3]) - min(VonKarajan[3])) / (2 * IQR * len(VonKarajan[3])**(-1/3)))
+density = columns / (len(VonKarajan[3])*0.1)
 
-hist, bins = np.histogram(VonKarajan[3], nbins, density=False)
-bin_centers = (bins[:-1] + bins[1:]) / 2
-bin_widths = np.diff(bins)
-density = hist / (len(VonKarajan[3]) * bin_widths[0])
-
-plt.bar(bins[:-1], density, width=bin_widths, alpha=0.5, color='b', label=r'$P_{N}(r)^{num}$')
+plt.bar(bins, density, width=0.1, alpha=0.5, color='b', label=r'$P_{N}(r)^{num}$')
 plt.xlabel('r(N)')
 plt.ylabel('Probability Density')
-plt.legend()
 plt.grid()
+x = np.linspace(0, bins[-1], 1000)
+Rayleigh = lambda x: (2*x/32)* math.e **(-x**2 / 32)
+plt.plot(x, Rayleigh(x), label=r'$P_{N}(r)^{theo}$', color='black')
+plt.legend()
 plt.show()
 
 
 # Plot - Mean square position over N
-t = np.array([i for i in range(1,65)])
+t = np.array([i for i in range(1,33)])
 
 plt.scatter(t, VonKarajan[2], color='black')
 plt.xlabel('i')
@@ -43,13 +62,21 @@ plt.ylabel(r'$\langle (\Delta r_{i})^2 \rangle^{num}$', fontsize=12)
 plt.legend()
 plt.grid(True)
 plt.show()
-
-
+"""
+somma = 0
+N=8
+a=1
+b=1
+for i in range(a,N-b):
+    somma += math.comb(i, int((i+a)/2)) * math.comb(8-i, int((8-i+b)/2))
+    
+print(somma/ (4**N))
 
 # -----------------------------
 # ---- B) With unit random steps (on the unitary circle theta belonging to [0, 2PI[ )
 
-Mozart = RW2D_average(10000, 64, 0, 0, 0, 0, 0, True)
+"""
+Mozart = RW2D_average(5000, 64, 0, 0, 0, 0, 0, True)
 
 RW_2D_plot(Mozart, '2D_RW_continuous.gif')
 plt.close('all')
@@ -66,6 +93,9 @@ density = hist / (len(Mozart[3]) * bin_widths[0])
 plt.bar(bins[:-1], density, width=bin_widths, alpha=0.5, color='b', label=r'$P_{N}(r)^{num}$')
 plt.xlabel('r(N)')
 plt.ylabel('Probability Density')
+x = np.linspace(0, bins[-1], 1000)
+Rayleigh = lambda x: (2*x/64)* math.e **(-x**2 / 64)
+plt.plot(x, Rayleigh(x), label=r'$P_{N}(r)^{theo}$', color='black')
 plt.legend()
 plt.grid()
 plt.show()
@@ -80,7 +110,7 @@ plt.ylabel(r'$\langle (\Delta r_{i})^2 \rangle^{num}$', fontsize=12)
 plt.legend()
 plt.grid(True)
 plt.show()
-
+"""
 
 
 
