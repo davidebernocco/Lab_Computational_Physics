75e766356821997cd08114a6c82c4ec5bd338abf
Davide Bernocco <davide.plantageneto@gmail.com>
2024-01-18 22:10:57 +0100

    Add files via upload

--- week_7/Esercizi_settimana7.py
+++ week_7/Esercizi_settimana7.py
@@ -9,46 +9,16 @@ import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.rcParams['text.usetex'] = True
 import time
-from Funz7 import Metropolis
-from scipy.stats import norm
+from Funz7 import Metropolis, gauss_func, plot_histo
 
 
 sigma = 1
-n_step = 10000
+n_step = np.asarray([10**2, 10**3, 10**4, 10**5])
 
             
 start_time1 = time.time()
 
-x_lst = np.arange(n_step)
-y_lst = Metropolis( 0, 2*sigma, n_step, sigma)
-
-
-plt.plot(x_lst, y_lst )
-plt.xlabel('i step', fontsize=12)
-plt.ylabel(r'$ x_i $', fontsize=12)
-plt.grid(True)
-plt.show()
-
- 
-IQR = np.percentile(y_lst, 75) - np.percentile(y_lst, 25)
-nbins = int((max(y_lst) - min(y_lst)) / (2 * IQR * len(y_lst)**(-1/3)))
-
-hist, bins = np.histogram(y_lst, nbins, density=False)
-bin_centers = (bins[:-1] + bins[1:]) / 2
-bin_widths = np.diff(bins)
-density = hist / (n_step * bin_widths[0])
-
-plt.bar(bins[:-1], density, width=bin_widths, alpha=0.5, color='b', label=r'$ PDF^{num} $')
-
-x = np.linspace(min(y_lst), max(y_lst), 1000)
-y = norm.pdf(x, 0, sigma)
-plt.plot(x, y, label=r'$PDF^{theo}$', color='black')
-
-plt.xlabel('x', fontsize=12)
-plt.ylabel('Probability density', fontsize=12)
-plt.grid(True)
-plt.legend()
-plt.show()  
+parrot = plot_histo(n_step, Metropolis, sigma)
 
 end_time1 = time.time()
 elapsed_time1 = end_time1 - start_time1
--- week_7/Funz7.py
+++ week_7/Funz7.py
@@ -5,6 +5,9 @@ Library of self-made functions needed for the codes implemented for the exercise
 """
 import math
 import numpy as np
+from scipy.stats import norm
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
     
 
@@ -14,8 +17,13 @@ def proposal_distr(a, b): # Usually "Gaussian" or "uniform" distribution
 
 
 
+def gauss_func(x, mu, sigma):
+    return norm.pdf(x, mu, sigma)
+
+
 def Metropolis( x0, delta, n, s):
     
+    acc = 0
     points = np.zeros(n, dtype = np.float32)
     points[0] = x0
     
@@ -32,9 +40,63 @@ def Metropolis( x0, delta, n, s):
         
         if alpha >= np.random.rand() :
             x_t = x_star
+            acc += 1
             
         points[i] = x_t
             
-    return points
+    return points, acc/n
 
 
+
+def plot_histo(n_arr, func, s):
+    
+    coefficients = {}
+    
+    for i in range(len(n_arr)):
+        
+        x_lst = np.arange(n_arr[i])
+        y_lst = func( 0, 5*s, n_arr[i], s)[0]
+
+        plt.plot(x_lst, y_lst )
+        plt.xlabel('i step', fontsize=12)
+        plt.ylabel(r'$ x_i $', fontsize=12)
+        plt.grid(True)
+        plt.show()
+
+         
+        IQR = np.percentile(y_lst, 75) - np.percentile(y_lst, 25)
+        nbins = int((max(y_lst) - min(y_lst)) / (2 * IQR * len(y_lst)**(-1/3)))
+
+        hist, bins = np.histogram(y_lst, nbins, density=False)
+        bin_centers = (bins[:-1] + bins[1:]) / 2
+        bin_widths = np.diff(bins)
+        density = hist / (n_arr[i] * bin_widths[0])
+
+        plt.bar(bins[:-1], density, width=bin_widths, alpha=0.5, color='b', label=r'$ PDF^{num} $')
+
+
+
+        params, covariance = curve_fit(gauss_func, bin_centers, density)
+        Mu, Sigma = params
+        
+        expected_values = gauss_func(bin_centers, Mu, Sigma)
+        chi_square = np.sum((density - expected_values) ** 2 / expected_values)
+
+        x_range = np.linspace(min(bin_centers), max(bin_centers), 1000)
+        plt.plot(x_range, gauss_func(x_range, Mu, Sigma), label='Gauss fit', color='black')
+
+        plt.xlabel('x', fontsize=12)
+        plt.ylabel('Probability density', fontsize=12)
+        plt.grid(True)
+        plt.legend()
+        plt.show() 
+        
+        coefficients[n_arr[i]] = (params, covariance, chi_square)
+    
+    print(coefficients)
+        
+    return coefficients
+        
+        
+        
+        
