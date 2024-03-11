aed5f6b6e1784623ece142d54926d5476c9891c7
Davide Bernocco <davide.plantageneto@gmail.com>
2024-03-12 00:39:38 +0100

    Add files via upload

--- week_9/Funz9.py
+++ week_9/Funz9.py
@@ -61,30 +61,39 @@ def accumulation(No, Nv, beta, eqSteps, mcSteps):
     
     Ener = initial_energy(config)
     Mag = np.sum(config)
-    E, E2 =  np.zeros(mcSteps), np.zeros(mcSteps)
-    M, M2 =  np.zeros(mcSteps), np.zeros(mcSteps)
+    E, E2 =  np.zeros(int(mcSteps/(No*Nv))+1), np.zeros(int(mcSteps/(No*Nv))+1)
+    M, M2 =  np.zeros(int(mcSteps/(No*Nv))+1), np.zeros(int(mcSteps/(No*Nv))+1)
     E[0], E2[0], M[0], M2[0] = Ener, Ener * Ener, Mag, Mag * Mag
-      
-    for i in range(mcSteps):
+    
+    j= 1
+    
+    for i in range(1, mcSteps + 1):
         config, dE, dM = Ising_conditions(config, beta)
         Ener += dE
         Mag += dM
         
-        E[i] = Ener
-        E2[i] = Ener * Ener
-        M[i] = Mag
-        M2[i] = Mag * Mag
+        if i%L == 0:
+            E[j] = Ener
+            E2[j] = Ener * Ener
+            M[j] = Mag
+            M2[j] = Mag * Mag
+            j += 1
         
     return M / L, M2 / L, E / L, E2 / L
         
 
 
+Beta = 1.5
+neqs = 75000
+nmcs = 100000
+n1 = n2 = 23
 
-results = accumulation(10, 10, 1.5, 3500, 10000)
-Nsteps_lst = np.arange(10000)
+
+results = accumulation(n1, n2, Beta, neqs, nmcs)
+Nsteps_lst = np.arange(int(nmcs/(n1*n2))+1)
 fig_m, ax_m = plt.subplots(figsize=(6.2, 4.5))
 ax_m.plot(Nsteps_lst, results[0], marker='o' )
-ax_m.set_xlabel(r'$ MCsteps $', fontsize=15)
+ax_m.set_xlabel(r'$ MCsteps/100 $', fontsize=15)
 ax_m.set_ylabel(r'$ M / N $', fontsize=15)
 ax_m.grid(True)
 plt.show()
@@ -97,17 +106,59 @@ plt.show()
 def averaged_quantities(No, Nv, beta, eqSteps, mcSteps):
     
     val = accumulation(No, Nv, beta, eqSteps, mcSteps)
-    fatt_aver = 1 / (mcSteps)
+    fatt_aver = 1 / (len(val[0]))
     e_aver = np.sum(val[2]) * fatt_aver
     e2_aver = np.sum(val[3]) * fatt_aver
     m_aver = np.sum(val[0]) * fatt_aver
     mABS_aver = np.sum(abs(val[0])) * fatt_aver
     m2_aver = np.sum(val[1]) * fatt_aver
-    C = (e2_aver - e_aver * e_aver) * beta ** 2
-    X = (m2_aver - m_aver * m_aver) * beta 
+    C = (e2_aver - (No*Nv) * e_aver * e_aver) * beta ** 2 
+    X = (m2_aver - (No*Nv) * mABS_aver * mABS_aver) * beta
 
     return e_aver, mABS_aver, C, X
 
+
+
+def T_variation(No, Nv, T_m, T_M, d_T):
+    arrT = np.arange(T_m, T_M, d_T)
+    e, m = np.zeros(len(arrT)),  np.zeros(len(arrT))
+    c, x = np.zeros(len(arrT)), np.zeros(len(arrT))
+    for i in range(len(arrT)):
+        e[i], m[i], c[i], x[i] = averaged_quantities(No, Nv, 1/arrT[i], 10**6, 10**6)
+        
+    return e, m, c, x
+
+
+resultsT = T_variation(40, 40, 1, 4, 0.25)
+Nsteps_lst =  np.arange(1, 4, 0.25)
+
+fig_mT, ax_mT = plt.subplots(figsize=(6.2, 4.5))
+ax_mT.plot(Nsteps_lst, resultsT[1], marker='o' )
+ax_mT.set_xlabel(r'$ T [K] $', fontsize=15)
+ax_mT.set_ylabel(r'$ \langle |M| \rangle / N $', fontsize=15)
+ax_mT.grid(True)
+plt.show()
+
+fig_eT, ax_eT = plt.subplots(figsize=(6.2, 4.5))
+ax_eT.plot(Nsteps_lst, resultsT[0], marker='o' )
+ax_eT.set_xlabel(r'$ T [K] $', fontsize=15)
+ax_eT.set_ylabel(r'$ \langle E \rangle / N $', fontsize=15)
+ax_eT.grid(True)
+plt.show()
+
+fig_cT, ax_cT = plt.subplots(figsize=(6.2, 4.5))
+ax_cT.plot(Nsteps_lst, resultsT[2], marker='o' )
+ax_cT.set_xlabel(r'$ T [K] $', fontsize=15)
+ax_cT.set_ylabel(r'$ c_V / N $', fontsize=15)
+ax_cT.grid(True)
+plt.show()
+
+fig_xT, ax_xT = plt.subplots(figsize=(6.2, 4.5))
+ax_xT.plot(Nsteps_lst, resultsT[3], marker='o' )
+ax_xT.set_xlabel(r'$ T [K] $', fontsize=15)
+ax_xT.set_ylabel(r'$ \chi / N $', fontsize=15)
+ax_xT.grid(True)
+plt.show()
 
 
 
