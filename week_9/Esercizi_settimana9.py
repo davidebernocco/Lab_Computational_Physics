38f7c0a80130f57b3f20e36cf74a70a160e7eb31
Davide Bernocco <davide.plantageneto@gmail.com>
2024-03-13 13:12:13 +0100

    Add files via upload

--- week_9/Esercizi_settimana9.py
+++ week_9/Esercizi_settimana9.py
@@ -4,7 +4,107 @@
 
 import numpy as np
 from PIL import Image
-from ipywidgets import interact
+#from ipywidgets import interact
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+
+
+
+# ------------------------- ESEMPIO DA INTERNET -------------------------------
+def random_spin_lattice(N, M):
+    return np.random.choice([-1,1], size=(N,M))
+
+def display_spin_lattice(lattice):
+    return Image.fromarray(np.uint8((lattice + 1) * 0.5 * 255))
+
+lattice = random_spin_lattice(100, 100)
+
+# Define a colormap
+spin_color = mcolors.ListedColormap(['blue', 'red'])
+
+# Display the lattice using matplotlib
+plt.imshow(display_spin_lattice(lattice), cmap = spin_color)
+plt.axis('off')  # Turn off axis
+plt.show()
+
+"""
+
+def Ising_update(lattice, n, m, beta):
+    total = 0
+    N, M = lattice.shape
+    for i in range(n-1, n+2):
+        for j in range(m-1, m+2):
+             if i == n and j == m:
+                 continue
+             total += lattice[i % N, j % M]
+             
+    dE = 2 * lattice[n, m] * total
+    
+    if dE <= 0:
+        lattice[n, m] *= -1
+    elif np.exp(-dE * beta) > np.random.rand():
+        lattice[n, m] *= -1
+    
+
+
+def Ising_step(lattice, beta):
+    N, M = lattice.shape
+    for n_offset in range(2):
+        for m_offset in range(2):
+            for n in range(n_offset, N, 2):
+                for m in range(m_offset, M, 2):
+                    Ising_update(lattice, n, m, beta)
+    return lattice
+
+
+def display_sequence(images):
+    def _show(frame=(0, len(images) - 1)):
+        return display_spin_lattice(images[frame])
+    return interact(_show)
+
+# -----------------------------------------------------------------------------
+
+
+
+
+
+@njit
+def Metropolis_Boltzmann( v0, dvmax, n, kb, T, m):
+    
+    acc = 0
+    velocity = np.zeros(n, dtype = np.float32)
+    energy = np.zeros(n, dtype = np.float32)
+    velocity[0] = v0
+    energy[0] = (m / 2) * v0 ** 2
+    
+    E_t = (m / 2) * v0 ** 2
+    v_t = v0
+    
+    for i in range(1, n):
+        v_star = np.random.uniform(v_t - dvmax, v_t + dvmax)
+        E_star = (m / 2) * v_star ** 2
+        
+        esp1v = ( -m * v_star ** 2 / ( 2 * kb * T) )  
+        esp2v = ( -m * v_t ** 2 / ( 2 * kb * T) )    
+        alphav = math.e ** (esp1v - esp2v)           
+        
+        if alphav >= np.random.rand() :
+            v_t = v_star
+            acc += 1
+        
+        esp1E = ( - E_star / ( kb * T) )  
+        esp2E = ( - E_t / ( kb * T) )    
+        alphaE = math.e ** (esp1E - esp2E) 
+               
+        
+        if alphaE >= np.random.rand() :
+            E_t = E_star
+            
+        velocity[i] = v_t
+        energy[i] = E_t
+            
+    return velocity, energy, acc/n
+"""
 
 
 
--- week_9/Funz9.py
+++ week_9/Funz9.py
@@ -12,6 +12,9 @@ import math
 import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.rcParams['text.usetex'] = True
+import matplotlib.colors as mcolors
+from matplotlib.animation import FuncAnimation
+from matplotlib.animation import PillowWriter
 
 def random_spin_lattice(N, M):
     return np.random.choice([-1,1], size=(N,M))
@@ -83,6 +86,48 @@ def accumulation(No, Nv, beta, eqSteps, mcSteps):
         
 
 
+
+"""
+# ----------------------------------------------------------------------------
+# CONFIGURATION SEQUENCE ANIMATION
+# ----------------------------------------------------------------------------
+
+def display_spin_lattice(lattice):
+    return Image.fromarray(np.uint8((lattice + 1) * 0.5 * 255))
+
+
+def animation_Ising(No, Nv, beta, mcSteps, plot_name):
+    L = No * Nv
+    config = random_spin_lattice(No, Nv)
+    fig = plt.figure()
+    
+    spin_color = mcolors.ListedColormap(['blue', 'red'])
+    im = plt.imshow(display_spin_lattice(config), cmap=spin_color)
+
+    plt.title('2D Ising model (no H)')
+    plt.axis('off')
+    
+    metadata = dict(title='Movie', artist='codinglikened')
+    writer = PillowWriter(fps=10, metadata=metadata)
+    
+    with writer.saving(fig, plot_name, 100):
+        for i in range(1, mcSteps + 1):
+            config, _, _ = Ising_conditions(config, beta)
+            
+            if i % L == 0:
+                im.set_data(display_spin_lattice(config))
+                writer.grab_frame()
+                
+    return
+
+
+animation_Ising(30, 30, 0.5, 200000, 'Ising_2D.gif')
+plt.close('all')
+
+# -----------------------------------------------------------------------------
+"""
+
+"""
 Beta = 1.5
 neqs = 75000
 nmcs = 100000
@@ -159,7 +204,7 @@ ax_xT.set_xlabel(r'$ T [K] $', fontsize=15)
 ax_xT.set_ylabel(r'$ \chi / N $', fontsize=15)
 ax_xT.grid(True)
 plt.show()
-
+"""
 
 
 
@@ -179,97 +224,12 @@ def block_average(lst, s):
         
     return Sigma_s / math.sqrt(s)
 
-
-
-
-
-
-
-# ------------------------- ESEMPIO DA INTERNET -------------------------------
-def random_spin_lattice(N, M):
-    return np.random.choice([-1,1], size=(N,M))
-
-def display_spin_lattice(lattice):
-    return Image.fromarray(np.uint8((lattice + 1) * 0.5 * 255))
-
-
-def Ising_update(lattice, n, m, beta):
-    total = 0
-    N, M = lattice.shape
-    for i in range(n-1, n+2):
-        for j in range(m-1, m+2):
-             if i == n and j == m:
-                 continue
-             total += lattice[i % N, j % M]
-             
-    dE = 2 * lattice[n, m] * total
-    
-    if dE <= 0:
-        lattice[n, m] *= -1
-    elif np.exp(-dE * beta) > np.random.rand():
-        lattice[n, m] *= -1
-    
-
-
-def Ising_step(lattice, beta):
-    N, M = lattice.shape
-    for n_offset in range(2):
-        for m_offset in range(2):
-            for n in range(n_offset, N, 2):
-                for m in range(m_offset, M, 2):
-                    Ising_update(lattice, n, m, beta)
-    return lattice
-
-
-def display_sequence(images):
-    def _show(frame=(0, len(images) - 1)):
-        return display_spin_lattice(images[frame])
-    return interact(_show)
-
-# -----------------------------------------------------------------------------
-
-
-
-
-
-@njit
-def Metropolis_Boltzmann( v0, dvmax, n, kb, T, m):
-    
-    acc = 0
-    velocity = np.zeros(n, dtype = np.float32)
-    energy = np.zeros(n, dtype = np.float32)
-    velocity[0] = v0
-    energy[0] = (m / 2) * v0 ** 2
-    
-    E_t = (m / 2) * v0 ** 2
-    v_t = v0
-    
-    for i in range(1, n):
-        v_star = np.random.uniform(v_t - dvmax, v_t + dvmax)
-        E_star = (m / 2) * v_star ** 2
-        
-        esp1v = ( -m * v_star ** 2 / ( 2 * kb * T) )  
-        esp2v = ( -m * v_t ** 2 / ( 2 * kb * T) )    
-        alphav = math.e ** (esp1v - esp2v)           
-        
-        if alphav >= np.random.rand() :
-            v_t = v_star
-            acc += 1
-        
-        esp1E = ( - E_star / ( kb * T) )  
-        esp2E = ( - E_t / ( kb * T) )    
-        alphaE = math.e ** (esp1E - esp2E) 
-               
-        
-        if alphaE >= np.random.rand() :
-            E_t = E_star
-            
-        velocity[i] = v_t
-        energy[i] = E_t
-            
-    return velocity, energy, acc/n
-
 """
+
+
+
+
+
 
 
 
