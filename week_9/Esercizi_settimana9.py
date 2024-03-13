6adccc1b9521ea4020877207e032d86f0b52c50b
Davide Bernocco <davide.plantageneto@gmail.com>
2024-03-13 19:22:05 +0100

    Add files via upload

--- week_9/Esercizi_settimana9.py
+++ week_9/Esercizi_settimana9.py
@@ -4,107 +4,147 @@
 
 import numpy as np
 from PIL import Image
-#from ipywidgets import interact
+from ipywidgets import interact
+from numba import njit
+import math
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.rcParams['text.usetex'] = True
 import matplotlib.colors as mcolors
+from matplotlib.animation import FuncAnimation
+from matplotlib.animation import PillowWriter
+
+from Funz9 import random_spin_lattice, display_spin_lattice, initial_energy
+from Funz9 import ordered_spin_lattice, Ising_conditions, accumulation
+from Funz9 import Ising_conditions_open, initial_energy_open, accumulation_open
+from Funz9 import animation_Ising, block_average,averaged_quantities
+from Funz9 import average_error, T_variation
 
 
 
-# ------------------------- ESEMPIO DA INTERNET -------------------------------
-def random_spin_lattice(N, M):
-    return np.random.choice([-1,1], size=(N,M))
-
-def display_spin_lattice(lattice):
-    return Image.fromarray(np.uint8((lattice + 1) * 0.5 * 255))
-
-lattice = random_spin_lattice(100, 100)
-
-# Define a colormap
-spin_color = mcolors.ListedColormap(['blue', 'red'])
-
-# Display the lattice using matplotlib
-plt.imshow(display_spin_lattice(lattice), cmap = spin_color)
-plt.axis('off')  # Turn off axis
-plt.show()
-
-"""
-
-def Ising_update(lattice, n, m, beta):
-    total = 0
-    N, M = lattice.shape
-    for i in range(n-1, n+2):
-        for j in range(m-1, m+2):
-             if i == n and j == m:
-                 continue
-             total += lattice[i % N, j % M]
-             
-    dE = 2 * lattice[n, m] * total
-    
-    if dE <= 0:
-        lattice[n, m] *= -1
-    elif np.exp(-dE * beta) > np.random.rand():
-        lattice[n, m] *= -1
-    
-
-
-def Ising_step(lattice, beta):
-    N, M = lattice.shape
-    for n_offset in range(2):
-        for m_offset in range(2):
-            for n in range(n_offset, N, 2):
-                for m in range(m_offset, M, 2):
-                    Ising_update(lattice, n, m, beta)
-    return lattice
-
-
-def display_sequence(images):
-    def _show(frame=(0, len(images) - 1)):
-        return display_spin_lattice(images[frame])
-    return interact(_show)
 
 # -----------------------------------------------------------------------------
+# STUDY OF THE EQUILIBRATION TIME
+# -----------------------------------------------------------------------------
+
+Beta = 1
+neqs = 0
+nmcs = 10**6
+n1 = n2 = 30
+
+
+results = accumulation(n1, n2, Beta, neqs, nmcs)
+Nsteps_lst = np.arange(int(nmcs/(n1*n2))+1)
+
+fig_m, ax_m = plt.subplots(figsize=(6.2, 4.5))
+ax_m.plot(Nsteps_lst, results[0], marker='o' )
+ax_m.set_xlabel(r'$ MCsteps/{} $'.format(n1*n2), fontsize=15)
+ax_m.set_ylabel(r'$ M / N $', fontsize=15)
+ax_m.grid(True)
+plt.show()
+
+fig_e, ax_e = plt.subplots(figsize=(6.2, 4.5))
+ax_e.plot(Nsteps_lst, results[2], marker='o' )
+ax_e.set_xlabel(r'$ MCsteps/{} $'.format(n1*n2), fontsize=15)
+ax_e.set_ylabel(r'$ E / N $', fontsize=15)
+ax_e.grid(True)
+plt.show()
 
 
 
 
 
-@njit
-def Metropolis_Boltzmann( v0, dvmax, n, kb, T, m):
-    
-    acc = 0
-    velocity = np.zeros(n, dtype = np.float32)
-    energy = np.zeros(n, dtype = np.float32)
-    velocity[0] = v0
-    energy[0] = (m / 2) * v0 ** 2
-    
-    E_t = (m / 2) * v0 ** 2
-    v_t = v0
-    
-    for i in range(1, n):
-        v_star = np.random.uniform(v_t - dvmax, v_t + dvmax)
-        E_star = (m / 2) * v_star ** 2
-        
-        esp1v = ( -m * v_star ** 2 / ( 2 * kb * T) )  
-        esp2v = ( -m * v_t ** 2 / ( 2 * kb * T) )    
-        alphav = math.e ** (esp1v - esp2v)           
-        
-        if alphav >= np.random.rand() :
-            v_t = v_star
-            acc += 1
-        
-        esp1E = ( - E_star / ( kb * T) )  
-        esp2E = ( - E_t / ( kb * T) )    
-        alphaE = math.e ** (esp1E - esp2E) 
-               
-        
-        if alphaE >= np.random.rand() :
-            E_t = E_star
-            
-        velocity[i] = v_t
-        energy[i] = E_t
-            
-    return velocity, energy, acc/n
-"""
+
+# -----------------------------------------------------------------------------
+# OPEN BOUNDARY CONDITIONS
+# -----------------------------------------------------------------------------
+
+Beta = 0.5
+neqs = 0
+nmcs = 10**7
+n1 = n2 = 30
+
+
+results_open = accumulation_open(n1, n2, Beta, neqs, nmcs)
+Nsteps_lst = np.arange(int(nmcs/(n1*n2))+1)
+
+fig_mo, ax_mo = plt.subplots(figsize=(6.2, 4.5))
+ax_mo.plot(Nsteps_lst, results_open[0], marker='o', label='Open Boundary Conditions')
+ax_mo.set_xlabel(r'$ MCsteps/{} $'.format(n1*n2), fontsize=15)
+ax_mo.set_ylabel(r'$ M / N $', fontsize=15)
+ax_mo.legend()
+ax_mo.grid(True)
+plt.show()
+
+fig_eo, ax_eo = plt.subplots(figsize=(6.2, 4.5))
+ax_eo.plot(Nsteps_lst, results_open[2], marker='o', label='Open Boundary Conditions')
+ax_eo.set_xlabel(r'$ MCsteps/{} $'.format(n1*n2), fontsize=15)
+ax_eo.set_ylabel(r'$ E / N $', fontsize=15)
+ax_eo.legend()
+ax_eo.grid(True)
+plt.show()
+
+
+
+
+
+
+# -----------------------------------------------------------------------------
+# PLOTTING PHYSICAL QUANTITIES: <|M|>/N, <E>/N, X/N, Cv/N
+# -----------------------------------------------------------------------------
+
+resultsT = T_variation(30, 30, 1, 4, 0.25, 10*5, 10**7, 100)
+Nsteps_lst =  np.arange(1, 4, 0.25)
+
+fig_mT, ax_mT = plt.subplots(figsize=(6.2, 4.5))
+ax_mT.scatter(Nsteps_lst, resultsT[0], marker='o', s=50)
+ax_mT.errorbar(Nsteps_lst, resultsT[0], yerr=resultsT[4], fmt='.',  capsize=5, color='black')
+ax_mT.set_xlabel(r'$ T [K] $', fontsize=15)
+ax_mT.set_ylabel(r'$ \langle |M| \rangle / N $', fontsize=15)
+ax_mT.grid(True)
+plt.show()
+
+fig_eT, ax_eT = plt.subplots(figsize=(6.2, 4.5))
+ax_eT.scatter(Nsteps_lst, resultsT[1], marker='o', s=50)
+ax_eT.errorbar(Nsteps_lst, resultsT[1], yerr=resultsT[5], fmt='.',  capsize=5, color='black')
+ax_eT.set_xlabel(r'$ T [K] $', fontsize=15)
+ax_eT.set_ylabel(r'$ \langle E \rangle / N $', fontsize=15)
+ax_eT.grid(True)
+plt.show()
+
+fig_cT, ax_cT = plt.subplots(figsize=(6.2, 4.5))
+ax_cT.scatter(Nsteps_lst, resultsT[2], marker='o', s=50)
+ax_cT.errorbar(Nsteps_lst, resultsT[2], yerr=resultsT[6], fmt='.',  capsize=5, color='black')
+ax_cT.set_xlabel(r'$ T [K] $', fontsize=15)
+ax_cT.set_ylabel(r'$ c_V / N $', fontsize=15)
+ax_cT.grid(True)
+plt.show()
+
+fig_xT, ax_xT = plt.subplots(figsize=(6.2, 4.5))
+ax_xT.scatter(Nsteps_lst, resultsT[3], marker='o', s=50)
+ax_xT.errorbar(Nsteps_lst, resultsT[3], yerr=resultsT[7], fmt='.',  capsize=5, color='black')
+ax_xT.set_xlabel(r'$ T [K] $', fontsize=15)
+ax_xT.set_ylabel(r'$ \chi / N $', fontsize=15)
+ax_xT.grid(True)
+plt.show()
+
+
+
+
+
+
+# -----------------------------------------------------------------------------
+# CONFIGURATION SEQUENCE ANIMATION
+# -----------------------------------------------------------------------------
+
+animation_Ising(30, 30, 1.5, 200000, 'Ising_2D.gif')
+plt.close('all')
+
+
+
+
+
+
 
 
 
--- week_9/Funz9.py
+++ week_9/Funz9.py
@@ -16,6 +16,11 @@ import matplotlib.colors as mcolors
 from matplotlib.animation import FuncAnimation
 from matplotlib.animation import PillowWriter
 
+
+
+
+
+
 def random_spin_lattice(N, M):
     return np.random.choice([-1,1], size=(N,M))
 
@@ -109,32 +114,6 @@ def accumulation(No, Nv, beta, eqSteps, mcSteps):
     """  
     return M / L, M2 / L, E / L, E2 / L
 
-
-
-"""
-Beta = 1
-neqs = 0
-nmcs = 10**6
-n1 = n2 = 30
-
-
-results = accumulation(n1, n2, Beta, neqs, nmcs)
-Nsteps_lst = np.arange(int(nmcs/(n1*n2))+1)
-
-fig_m, ax_m = plt.subplots(figsize=(6.2, 4.5))
-ax_m.plot(Nsteps_lst, results[0], marker='o' )
-ax_m.set_xlabel(r'$ MCsteps/{} $'.format(n1*n2), fontsize=15)
-ax_m.set_ylabel(r'$ M / N $', fontsize=15)
-ax_m.grid(True)
-plt.show()
-
-fig_e, ax_e = plt.subplots(figsize=(6.2, 4.5))
-ax_e.plot(Nsteps_lst, results[2], marker='o' )
-ax_e.set_xlabel(r'$ MCsteps/{} $'.format(n1*n2), fontsize=15)
-ax_e.set_ylabel(r'$ E / N $', fontsize=15)
-ax_e.grid(True)
-plt.show()
-"""
 
 
 
@@ -241,49 +220,11 @@ def accumulation_open(No, Nv, beta, eqSteps, mcSteps):
 
 
 
-Beta = 0.5
-neqs = 0
-nmcs = 10**7
-n1 = n2 = 30
-
-
-results_open = accumulation_open(n1, n2, Beta, neqs, nmcs)
-Nsteps_lst = np.arange(int(nmcs/(n1*n2))+1)
-
-fig_mo, ax_mo = plt.subplots(figsize=(6.2, 4.5))
-ax_mo.plot(Nsteps_lst, results_open[0], marker='o', label='Open Boundary Conditions')
-ax_mo.set_xlabel(r'$ MCsteps/{} $'.format(n1*n2), fontsize=15)
-ax_mo.set_ylabel(r'$ M / N $', fontsize=15)
-ax_mo.legend()
-ax_mo.grid(True)
-plt.show()
-
-fig_eo, ax_eo = plt.subplots(figsize=(6.2, 4.5))
-ax_eo.plot(Nsteps_lst, results_open[2], marker='o', label='Open Boundary Conditions')
-ax_eo.set_xlabel(r'$ MCsteps/{} $'.format(n1*n2), fontsize=15)
-ax_eo.set_ylabel(r'$ E / N $', fontsize=15)
-ax_eo.legend()
-ax_eo.grid(True)
-plt.show()
-
-# -----------------------------------------------------------------------------
-
-
-
-
-
-
-
-
-
 
 
 # ----------------------------------------------------------------------------
 # CONFIGURATION SEQUENCE ANIMATION
 # ----------------------------------------------------------------------------
-"""
-def display_spin_lattice(lattice):
-    return Image.fromarray(np.uint8((lattice + 1) * 0.5 * 255))
 
 
 def animation_Ising(No, Nv, beta, mcSteps, plot_name):
@@ -312,11 +253,6 @@ def animation_Ising(No, Nv, beta, mcSteps, plot_name):
     return
 
 
-animation_Ising(30, 30, 1.5, 200000, 'Ising_2D.gif')
-plt.close('all')
-"""
-# -----------------------------------------------------------------------------
-
 
 
 
@@ -325,7 +261,7 @@ plt.close('all')
 # PLOTTING PHYSICAL QUANTITIES: <|M|>/N, <E>/N, X/N, Cv/N
 # -----------------------------------------------------------------------------
 
-"""
+
 @njit
 def block_average(lst, s):
     
@@ -381,50 +317,6 @@ def T_variation(No, Nv, T_m, T_M, d_T, eqSteps, mcSteps, s):
         s_e[i], s_m[i], s_c[i], s_x[i] = average_error(data, No, Nv, 1/arrT[i], eqSteps, mcSteps, s)
         
     return m, e, c, x, s_m, s_e, s_c, s_x
-
-
-resultsT = T_variation(30, 30, 1, 4, 0.25, 10*5, 10**7, 100)
-Nsteps_lst =  np.arange(1, 4, 0.25)
-
-fig_mT, ax_mT = plt.subplots(figsize=(6.2, 4.5))
-ax_mT.scatter(Nsteps_lst, resultsT[0], marker='o', s=50)
-ax_mT.errorbar(Nsteps_lst, resultsT[0], yerr=resultsT[4], fmt='.',  capsize=5, color='black')
-ax_mT.set_xlabel(r'$ T [K] $', fontsize=15)
-ax_mT.set_ylabel(r'$ \langle |M| \rangle / N $', fontsize=15)
-ax_mT.grid(True)
-plt.show()
-
-fig_eT, ax_eT = plt.subplots(figsize=(6.2, 4.5))
-ax_eT.scatter(Nsteps_lst, resultsT[1], marker='o', s=50)
-ax_eT.errorbar(Nsteps_lst, resultsT[1], yerr=resultsT[5], fmt='.',  capsize=5, color='black')
-ax_eT.set_xlabel(r'$ T [K] $', fontsize=15)
-ax_eT.set_ylabel(r'$ \langle E \rangle / N $', fontsize=15)
-ax_eT.grid(True)
-plt.show()
-
-fig_cT, ax_cT = plt.subplots(figsize=(6.2, 4.5))
-ax_cT.scatter(Nsteps_lst, resultsT[2], marker='o', s=50)
-ax_cT.errorbar(Nsteps_lst, resultsT[2], yerr=resultsT[6], fmt='.',  capsize=5, color='black')
-ax_cT.set_xlabel(r'$ T [K] $', fontsize=15)
-ax_cT.set_ylabel(r'$ c_V / N $', fontsize=15)
-ax_cT.grid(True)
-plt.show()
-
-fig_xT, ax_xT = plt.subplots(figsize=(6.2, 4.5))
-ax_xT.scatter(Nsteps_lst, resultsT[3], marker='o', s=50)
-ax_xT.errorbar(Nsteps_lst, resultsT[3], yerr=resultsT[7], fmt='.',  capsize=5, color='black')
-ax_xT.set_xlabel(r'$ T [K] $', fontsize=15)
-ax_xT.set_ylabel(r'$ \chi / N $', fontsize=15)
-ax_xT.grid(True)
-plt.show()
-"""
-# -----------------------------------------------------------------------------
-
-
-
-
-
-
 
 
 
