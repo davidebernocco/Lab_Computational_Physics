5c6c3792908357a10c0213e528668fab7307e39f
Davide Bernocco <davide.plantageneto@gmail.com>
2024-03-13 14:03:55 +0100

    Add files via upload

--- week_9/Funz9.py
+++ week_9/Funz9.py
@@ -83,15 +83,34 @@ def accumulation(No, Nv, beta, eqSteps, mcSteps):
             j += 1
         
     return M / L, M2 / L, E / L, E2 / L
-        
-
 
 
 """
+
+Beta = 1.5
+neqs = 10**6
+nmcs = 10**6
+n1 = n2 = 20
+
+
+results = accumulation(n1, n2, Beta, neqs, nmcs)
+Nsteps_lst = np.arange(int(nmcs/(n1*n2))+1)
+fig_m, ax_m = plt.subplots(figsize=(6.2, 4.5))
+ax_m.plot(Nsteps_lst, results[0], marker='o' )
+ax_m.set_xlabel(r'$ MCsteps/100 $', fontsize=15)
+ax_m.set_ylabel(r'$ M / N $', fontsize=15)
+ax_m.grid(True)
+plt.show()
+
+"""
+
+
+
+
 # ----------------------------------------------------------------------------
 # CONFIGURATION SEQUENCE ANIMATION
 # ----------------------------------------------------------------------------
-
+"""
 def display_spin_lattice(lattice):
     return Image.fromarray(np.uint8((lattice + 1) * 0.5 * 255))
 
@@ -123,69 +142,90 @@ def animation_Ising(No, Nv, beta, mcSteps, plot_name):
 
 animation_Ising(30, 30, 0.5, 200000, 'Ising_2D.gif')
 plt.close('all')
+"""
+# -----------------------------------------------------------------------------
+
+
+
+
 
 # -----------------------------------------------------------------------------
-"""
-
-"""
-Beta = 1.5
-neqs = 75000
-nmcs = 100000
-n1 = n2 = 23
+# PLOTTING PHYSICAL QUANTITIES: <|M|>/N, <E>/N, X/N, Cv/N
+# -----------------------------------------------------------------------------
 
 
-results = accumulation(n1, n2, Beta, neqs, nmcs)
-Nsteps_lst = np.arange(int(nmcs/(n1*n2))+1)
-fig_m, ax_m = plt.subplots(figsize=(6.2, 4.5))
-ax_m.plot(Nsteps_lst, results[0], marker='o' )
-ax_m.set_xlabel(r'$ MCsteps/100 $', fontsize=15)
-ax_m.set_ylabel(r'$ M / N $', fontsize=15)
-ax_m.grid(True)
-plt.show()
-
-
-
-
-
-
-def averaged_quantities(No, Nv, beta, eqSteps, mcSteps):
+@njit
+def block_average(lst, s):
     
-    val = accumulation(No, Nv, beta, eqSteps, mcSteps)
-    fatt_aver = 1 / (len(val[0]))
-    e_aver = np.sum(val[2]) * fatt_aver
-    e2_aver = np.sum(val[3]) * fatt_aver
-    m_aver = np.sum(val[0]) * fatt_aver
-    mABS_aver = np.sum(abs(val[0])) * fatt_aver
-    m2_aver = np.sum(val[1]) * fatt_aver
-    C = (e2_aver - (No*Nv) * e_aver * e_aver) * beta ** 2 
-    X = (m2_aver - (No*Nv) * mABS_aver * mABS_aver) * beta
+    aver = np.zeros(s, dtype = np.float32)
+    block_size = int(len(lst) / s)
+
+    for k in range(s):
+        aver[k] = np.std(lst[(k * block_size):((k + 1) * block_size)])
+        
+    Sigma_s = np.std(aver)
+        
+    return Sigma_s / math.sqrt(s)
+
+
+
+def averaged_quantities(data, No, Nv, beta, eqSteps, mcSteps):
+    L = No*Nv
+    fatt_aver = 1 / (len(data[0]))
+    e_aver = np.sum(data[2]) * fatt_aver
+    e2_aver = np.sum(data[3]) * fatt_aver
+    m_aver = np.sum(data[0]) * fatt_aver
+    mABS_aver = np.sum(abs(data[0])) * fatt_aver
+    m2_aver = np.sum(data[1]) * fatt_aver
+    C = (e2_aver - L * e_aver * e_aver) * beta ** 2 
+    X = (m2_aver - L * mABS_aver * mABS_aver) * beta
 
     return e_aver, mABS_aver, C, X
 
 
+def average_error(data, No, Nv, beta, eqSteps, mcSteps, s):
+    L = No*Nv
+    aver = averaged_quantities(data, No, Nv, beta, eqSteps, mcSteps)
+    err_e = block_average(data[2],s)
+    err_e2 = block_average(data[3],s)
+    err_m = block_average(data[0],s)
+    err_m2 = block_average(data[1],s)
+    err_mABS = block_average(abs(data[0]), s)
+    err_C = math.sqrt(err_e2**2 + 4*L**2*aver[0]**2*err_e**2) * beta**2
+    err_X =  math.sqrt(err_m2**2 + 4*L**2*aver[1]**2*err_mABS**2) * beta
+    
+    return err_e, err_mABS, err_C, err_X
+    
 
-def T_variation(No, Nv, T_m, T_M, d_T):
+
+def T_variation(No, Nv, T_m, T_M, d_T, eqSteps, mcSteps, s):
     arrT = np.arange(T_m, T_M, d_T)
     e, m = np.zeros(len(arrT)),  np.zeros(len(arrT))
     c, x = np.zeros(len(arrT)), np.zeros(len(arrT))
+    s_e, s_m =  np.zeros(len(arrT)),  np.zeros(len(arrT))
+    s_c, s_x =  np.zeros(len(arrT)),  np.zeros(len(arrT))
+    
     for i in range(len(arrT)):
-        e[i], m[i], c[i], x[i] = averaged_quantities(No, Nv, 1/arrT[i], 10**6, 10**6)
+        data = accumulation(No, Nv, 1/arrT[i], eqSteps, mcSteps)
+        e[i], m[i], c[i], x[i] = averaged_quantities(data, No, Nv, 1/arrT[i], eqSteps, mcSteps)
+        s_e[i], s_m[i], s_c[i], s_x[i] = average_error(data, No, Nv, 1/arrT[i], eqSteps, mcSteps, s)
         
-    return e, m, c, x
+    return m, e, c, x, s_m, s_e, s_c, s_x
 
 
-resultsT = T_variation(40, 40, 1, 4, 0.25)
+resultsT = T_variation(10, 10, 1, 4, 0.25, 10**6, 10**6, 10)
 Nsteps_lst =  np.arange(1, 4, 0.25)
 
 fig_mT, ax_mT = plt.subplots(figsize=(6.2, 4.5))
-ax_mT.plot(Nsteps_lst, resultsT[1], marker='o' )
+ax_mT.scatter(Nsteps_lst, resultsT[0], marker='o', s=50)
+ax_mT.errorbar(Nsteps_lst, resultsT[0], yerr=resultsT[4], fmt='.',  capsize=5, color='black')
 ax_mT.set_xlabel(r'$ T [K] $', fontsize=15)
 ax_mT.set_ylabel(r'$ \langle |M| \rangle / N $', fontsize=15)
 ax_mT.grid(True)
 plt.show()
-
+"""
 fig_eT, ax_eT = plt.subplots(figsize=(6.2, 4.5))
-ax_eT.plot(Nsteps_lst, resultsT[0], marker='o' )
+ax_eT.plot(Nsteps_lst, resultsT[1], marker='o' )
 ax_eT.set_xlabel(r'$ T [K] $', fontsize=15)
 ax_eT.set_ylabel(r'$ \langle E \rangle / N $', fontsize=15)
 ax_eT.grid(True)
@@ -205,26 +245,8 @@ ax_xT.set_ylabel(r'$ \chi / N $', fontsize=15)
 ax_xT.grid(True)
 plt.show()
 """
+# -----------------------------------------------------------------------------
 
-
-
-
-
-"""
-@njit
-def block_average(lst, s):
-    
-    aver = np.zeros(s, dtype = np.float32)
-    block_size = int(len(lst) / s)
-
-    for k in range(s):
-        aver[k] = np.std(lst[(k * block_size):((k + 1) * block_size)])
-        
-    Sigma_s = np.std(aver)
-        
-    return Sigma_s / math.sqrt(s)
-
-"""
 
 
 
