3751ba6ba5249e134c5bb0db3ca2b826f707ca7b
Davide Bernocco <davide.plantageneto@gmail.com>
2024-04-11 18:33:10 +0200

    Add files via upload

--- week_10/Esercizi_settimana10.py
+++ week_10/Esercizi_settimana10.py
@@ -3,12 +3,70 @@
 @author: david
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.rcParams['text.usetex'] = True
+from Funz10 import MC_iteration, block_average, aver_DT
 
 
 
 
+# ------------------------
+L = 100
+MCsteps = 2500
+Nsteps_lst =  np.arange(1, MCsteps + 1, 1)
+
+fig_dR2, ax_dR2 = plt.subplots(figsize=(6.2, 4.5))
+fig_D, ax_D = plt.subplots(figsize=(6.2, 4.5))
+
+for i in [0.03, 0.1, 0.2, 0.3, 0.5, 0.7]:
+    the = MC_iteration(L, L, int(i*L**2), MCsteps)
+    
+    ax_dR2.scatter(Nsteps_lst, the[0], marker='o', s=50, label=r'$ \rho = {} $'.format(i))
+    ax_D.scatter(Nsteps_lst, the[1], marker='o', s=50)
+    
+ax_dR2.set_xlabel(r'$ t [a.u.] $', fontsize=15)
+ax_dR2.set_ylabel(r'$ \langle \Delta R^2(t) \rangle $', fontsize=15)
+ax_dR2.grid(True)
+ax_dR2.legend()
+plt.show()  
+
+ax_D.set_xlabel(r'$t [a.u.] $', fontsize=15)
+ax_D.set_ylabel(r'$ D(t) $', fontsize=15)
+ax_D.grid(True)
+plt.show()
+    
+    
 
 
+# ----------------
+
+
+the = MC_iteration(30, 30, 27, 225)
+D_aver, sD_aver = block_average(the[1], 10)
+Nsteps_lst =  np.arange(1, 225 + 1, 1)
+
+fig_D, ax_D = plt.subplots(figsize=(6.2, 4.5))
+ax_D.scatter(Nsteps_lst, the[1], marker='o', s=50)
+ax_D.plot([1,225], [D_aver, D_aver], label=r'$ \langle D(t) \rangle_T $')
+ax_D.set_xlabel(r'$ t [a.u.] $', fontsize=15)
+ax_D.set_ylabel(r'$ D(t) $', fontsize=15)
+ax_D.grid(True)
+plt.show()
+
+
+the = MC_iteration(100, 100, 300, 2500)
+D_aver, sD_aver = block_average(the[1], 10)
+Nsteps_lst =  np.arange(1, 2500 + 1, 1)
+
+fig_D, ax_D = plt.subplots(figsize=(6.2, 4.5))
+ax_D.scatter(Nsteps_lst, the[1], marker='o', s=50)
+ax_D.plot([1,2500], [D_aver, D_aver], label=r'$ \langle D(t) \rangle_T $')
+ax_D.set_xlabel(r'$ t [a.u.] $', fontsize=15)
+ax_D.set_ylabel(r'$ D(t) $', fontsize=15)
+ax_D.grid(True)
+plt.show()
 
 
 
--- week_10/Funz10.py
+++ week_10/Funz10.py
@@ -87,19 +87,47 @@ def MC_iteration(Lo, Lv, Np, Nmc):
 @njit
 def block_average(lst, s):
     
-    aver = np.zeros(s, dtype = np.float32)
+    sigma = np.zeros(s, dtype = np.float32)
+    aver =  np.zeros(s, dtype = np.float32)
     block_size = int(len(lst) / s)
 
     for k in range(s):
-        aver[k] = np.std(lst[(k * block_size):((k + 1) * block_size)])
+        sigma[k] = np.std(lst[(k * block_size):((k + 1) * block_size)])
+        aver[k] = np.mean(lst[(k * block_size):((k + 1) * block_size)])
         
-    Sigma_s = np.std(aver)
+    Sigma_s = np.std(sigma)
+    Aver_tot = np.mean(aver)
         
-    return Sigma_s / math.sqrt(s)
+    return Aver_tot, Sigma_s / math.sqrt(s)
 
 
 
 
+def aver_DT(L, Np, Nmc, s, Naver):
+    D_aver = 0
+    sD_aver = 0
+    
+    for i in range(Naver):
+        Dt = MC_iteration(L, L, Np, Nmc)[1]
+        D, sD = block_average(Dt, s)
+        D_aver += D
+        sD_aver += sD**2
+        
+    return D_aver / Naver, math.sqrt(sD_aver)
+    
+
+
+# Non quadra quel che ottengo: che intende come fluttuazioni di D(t)??????
+def sD_N(L, r, Nmc, s):
+    sD_arr = np.zeros(len(L))
+    
+    for i in range(len(L)):
+        Np = int(r * L[i]**2)
+        Dt = MC_iteration(L[i], L[i], Np, Nmc[i])[1]
+        D, sD = block_average(Dt, s)
+        sD_arr[i] = sD
+        
+    return sD_arr
 
 
 
