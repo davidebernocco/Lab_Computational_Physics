6dcbf762d93f3736ce4b12a5c6ae0ce0e033e35f
Davide Bernocco <davide.plantageneto@gmail.com>
2024-04-27 23:15:13 +0200

    Add files via upload

--- week_7/Esercizi_settimana7.py
+++ week_7/Esercizi_settimana7.py
@@ -1,5 +1,5 @@
 """
-Now I have to do everything from the beginning again
+Plots and other numerical estimations (7th week)
 
 @author: david
 """
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.rcParams['text.usetex'] = True
 import time
-from Funz7 import Metropolis, gauss_func, plot_histo, acc_ratio
+from Funz7 import Metropolis, plot_histo, acc_ratio
 from Funz7 import equil, accuracy, dir_sampl_ground_state
 from Funz7 import Metro_sampl_ground_state, corr, boxmuller
 from Funz7 import Metropolis_Boltzmann, Metro_sampl_Boltzmann
@@ -17,16 +17,19 @@ from Funz7 import Metropolis_Boltzmann_N, Metro_sampl_Boltzmann_N
 
 
 
-#-- ES 1 --
-#---------- Random numbers with gaussian distribution: the METROPOLIS algorithm
+# -----------------------------------------------------------------------------
+# RANDOM NUMBERS WITH GAUSSIAN DISTRIBUTION: METROPOLIS MONTE CARLO ALGORITHM
+# -----------------------------------------------------------------------------
 
 
-# ---- 1.1) Which n gives a satisfactory accordance with expected result?
-"""
+
+# --------------------------
+# 1.1) Find the number n of sampling points that gives a satisfactory
+# accordance with expected result
+
 sigma = 1
 n_step = np.asarray([10**2, 10**3, 10**4, 10**5])
 
-            
 start_time1 = time.time()
 
 parrot = plot_histo(n_step, Metropolis, sigma, 5*sigma)
@@ -34,12 +37,14 @@ parrot = plot_histo(n_step, Metropolis, sigma, 5*sigma)
 end_time1 = time.time()
 elapsed_time1 = end_time1 - start_time1
 print(f"Elapsed time1: {elapsed_time1:.4f} seconds")
-"""
 
 
 
-# ---- 1.2) Dependence of acceptance ratio on delta, keeping n fixed
-"""
+
+
+# --------------------------
+# 1.2) Study of the dependence of acceptance ratio on delta (keeping n fixed)
+
 sigma = 1
 
 delta_arr = np.asarray([0.5*sigma + i for i in range(10)])
@@ -55,12 +60,15 @@ plt.ylabel(' Acceptance ratio ', fontsize=12)
 plt.grid(True)
 plt.legend()
 plt.show()
-"""
 
 
 
-#  ---- 1.3) |Numerical variance - exact variance| vs n: Equilibration time
-"""
+
+
+# --------------------------
+# 1.3) Estimation of Equilibration time.
+# plots |Numerical variance - exact variance| vs the number of samples n: 
+
 num_arr = np.arange(100, 10100, 100)
 num_dependence = equil(0, 5, 10000, 1, 100, 100)
 
@@ -73,19 +81,23 @@ ax_eq.set_xlabel('n', fontsize=15)
 ax_eq.set_ylabel(r'$ | \sigma_{num}^2 - \sigma_{exp}^2 | / \sigma_{exp}^2 $', fontsize=15)
 ax_eq.grid(True)
 plt.show()
-"""
 
 
 
 
 
 
-#-- ES 2 --
-#---------- Sampling physical quantities: direct sampling and METROPOLIS SAMPLING
+
+# -----------------------------------------------------------------------------
+# SAMPLING PHYSICAL QUANTITIES: DIRECT SAMPLING AND METROPOLIS SAMPLING
+# -----------------------------------------------------------------------------
 
 
-# ---- 2.1), 2.2) Direct sampling
-"""
+#  2.1), 2.2) Evaluates an integral corresponding to expectation value of a 
+# certain physical quantities through sample mean method with gaussian 
+# sampled points (Box-Muller). Plots actual errors
+
+
 lista_n = np.asarray([2 ** i for i in range(7, 20)])
 acc = accuracy(1, lista_n, dir_sampl_ground_state)
 
@@ -98,11 +110,13 @@ plt.ylabel(r'$ \log(\Delta_{n}) $', fontsize=12)
 plt.legend(loc='lower right')
 plt.grid(True)
 plt.show()
-"""
 
 
-# ---- 2.3) Metropolis sampling
-"""
+
+
+
+# 2.3) The same as before, but now it uses Metropolis sampling
+
 lista_n = np.asarray([2 ** i for i in range(7, 20)])
 acc_Metro = accuracy(1, lista_n, Metro_sampl_ground_state)
 
@@ -115,17 +129,23 @@ plt.ylabel(r'$ \log(\Delta_{n}) $', fontsize=12)
 plt.legend(loc='lower left')
 plt.grid(True)
 plt.show()
-"""
 
 
 
-#-- ES 3 --
-#---------- CORRELATIONS 
 
 
-# ---- 3.1) Correlations for n fixed, varying delta
-"""
-delta_arr = [0.5, 1, 5, 10, 15] #0.5,1,5,8,10
+
+# -----------------------------------------------------------------------------
+# CORRELATIONS
+# -----------------------------------------------------------------------------
+
+# Since the Metropolis MC algorithm belongs to the class of Markov Chain MC 
+# methods, correlations are present within the sample.
+
+
+# 3.1) Plots correlations for n fixed, varying Metropolis parameter delta
+
+delta_arr = [0.5, 1, 5, 10, 15]
 
 for i in range(len(delta_arr)):
     dracula = Metropolis(0, delta_arr[i], 10000, 1)[0]
@@ -138,11 +158,13 @@ plt.ylabel(r'$ C(j) $', fontsize=12)
 plt.legend(loc='upper right')
 plt.grid(True)
 plt.show()
-"""
 
 
-# ---- 3.2) Correlations comparison: Box-Muller vs Metropolis
-"""
+
+
+
+# 3.2) Compares the correlations behaviour of  Box-Muller and Metropolis methods
+
 house = boxmuller(10000)
 garden = Metropolis(0, 5, 10000, 1)[0]
 
@@ -156,23 +178,23 @@ plt.ylabel(r'$ C(j) $', fontsize=12)
 plt.legend(loc='upper right')
 plt.grid(True)
 plt.show()
-"""
 
 
 
 
 
-#-- ES 4 --
-#---------- Verification of the BOLTZMAN DISTRIBUTION
+
+# -----------------------------------------------------------------------------
+# VERIFICATION OF THE BOLTZMANN DISTRIBUTION
+# -----------------------------------------------------------------------------
 
 
-# ---- 4.1), 4.3) SINGLE CLASSICAL PARTICLE 1D IN THERMAL EQUILIBRIUM
+#  4.1), 4.3) Build the histogram corresponding to both the velocity and
+# energy distribution of a single particle in 1D in thermal equilibrium.
 
-"""
 npoints = 100000
 
 MB_distr1D = Metropolis_Boltzmann(0, 10, npoints, 1, 1, 1)
-
 
 
 IQR = np.percentile(MB_distr1D[0], 75) - np.percentile(MB_distr1D[0], 25)
@@ -191,7 +213,6 @@ ax_v.grid(True)
 plt.show() 
 
 
-
 IQRE = np.percentile(MB_distr1D[1], 75) - np.percentile(MB_distr1D[1], 25)
 nbinsE = int((max(MB_distr1D[1]) - min(MB_distr1D[1])) / (2 * IQRE * len(MB_distr1D[1])**(-1/3)))
 
@@ -208,16 +229,17 @@ ax_E.grid(True)
 plt.show() 
 
 
-
 cri_cri = Metro_sampl_Boltzmann(0, 10, npoints, 1, 1, 1, int(npoints/1000))
 print( "Mean velocity= ", cri_cri[0], "+/-", cri_cri[2]) 
 print( "Mean Energy", cri_cri[1] / 2, "+/-", cri_cri[3]/2)
 print( "Expected values: 0, (kb * T) / 2 ")
-"""
 
 
-# ---- 4.2) Check P(E) follows the expected behavoir
-"""
+
+
+
+# 4.2) Checks that P(E) follows the expected behaviour.
+
 fig_lnE, ax_lnE = plt.subplots(figsize=(6.2, 4.5))
 ax_lnE.scatter(np.log(bin_centersE), np.log(densityE), color='g', label=r'$ ln( P(E)_{num}) $',  marker='o', s=50)
 ax_lnE.plot(np.log(bin_centersE), np.log(list(map(lambda x: (1/np.sqrt(math.pi*x))*math.e**(-x), bin_centersE))), color='red', label=r'$ ln( P(E)_{theo}) $' ) 
@@ -226,28 +248,35 @@ ax_lnE.set_ylabel(r'$ ln( P(E)) $', fontsize=12)
 ax_lnE.grid(True)
 ax_lnE.legend()
 plt.show() 
-"""
 
 
 
-# ---- 4.5) IDEAL 1D CLASSICAL GAS OF N PARTICLES IN THERMAL EQUILIBRIUM
 
-"""
+
+# 4.5) Output the mean velocity and mean energy of an ideal 1D classical gas 
+# of N particles in thermal equilibrium.
+
 n_step = 100000
 N_part = 5
 
+"""
 # Mean vel tends to zero beacause of symmetry (no preferential direction +/- x)
 # Mean energy changes in time due to typical canonical E fluctuation. However stay always around <E>
+"""
 
 miao = Metro_sampl_Boltzmann_N(10, 30, n_step, 1, 100, 1, N_part, int((N_part * n_step)/20000))
 print( "Mean particle velocity = ", miao[0], "+/-", miao[2])
 print( "Mean particle Energy", miao[1]/2, "+/-", miao[3]/2)
 print( "Expected values: 0, (kb * T) / 2")
 print( "\n", "The acceptance ratio depends on T and delta!!")
-"""
 
-# ---- 4.6) P(<E>/N) (Should asymptotically tends to a gaussian centered in the <E> over all microstates)
-"""
+
+
+
+
+# 4.6) Verifies that, for N->Inf, P(<E>/N) asymptotically tends to a gaussian 
+# distribution centered in <E>/N ( <.> is for average over all microstates)
+
 n_step = 100000
 N_part = 1000
 
@@ -270,11 +299,14 @@ ax_EN.set_ylabel('Probability density', fontsize=15)
 ax_EN.grid(True)
 ax_EN.legend()
 plt.show() 
-"""
 
 
-# 4.7) ---- Heat capacity (should be constant!)
-"""
+
+
+
+# 4.7) It checks if the heat capacity of an ideal classical gas is constant
+# Numerical derivative method is implemented for a qualitative raw estimation
+
 n_step = 100000
 N_part = 200
 
@@ -288,11 +320,14 @@ print((np.mean(mano2[1]) - np.mean(mano1[1])) / 10)
 print((np.mean(mano3[1]) - np.mean(mano2[1])) / 10)
 print((np.mean(mano10[1]) - np.mean(mano9[1])) / 10)
 print((np.mean(mano11[1]) - np.mean(mano10[1])) / 10)
-"""
 
-# 4.8) Mean square energy fluctuations
 
-"""
+
+
+
+# 4.8) A peculiar phenomenon occuring in the canonical ensemble is studied:
+# the mean square energy fluctuations behave like 1/sqrt(N)
+
 lista_N = np.asarray([2**i for i in range(6,12)])
 fluct = np.zeros(len(lista_N), dtype = np.float32)
 for i in range(len(lista_N)):
@@ -306,6 +341,6 @@ ax_fl.set_xlabel(r'$ ln(N) $', fontsize=15)
 ax_fl.set_ylabel(r'$ ln(\langle \Delta E^2 \rangle / \langle E \rangle) $', fontsize=15)
 ax_fl.grid(True)
 plt.show() 
-"""
+
 
 
--- week_7/Funz7.py
+++ week_7/Funz7.py
@@ -1,5 +1,5 @@
 """
-Library of self-made functions needed for the codes implemented for the exercises of the 5th week
+Library of self-made functions needed for the 7th week exercises
 
 @author: david
 """
@@ -11,34 +11,42 @@ from scipy.optimize import curve_fit
 from numba import njit
 import random
 
-    
 
-def proposal_distr(a, b): # Usually "Gaussian" or "uniform" distribution
+# -----------------------------------------------------------------------------
+# RANDOM NUMBERS WITH GAUSSIAN DISTRIBUTION: METROPOLIS MONTE CARLO ALGORITHM
+# -----------------------------------------------------------------------------
+
+
+# Proposal distribution within the Metropolis algorith.
+# Usually it is a gaussian or a uniform distribution (like here)
+def proposal_distr(a, b):
     
     return np.random.uniform(a, b, 1)
 
 
 
+
+
+# Gaussian test function 
 def gauss_func(x, mu, sigma):
     return norm.pdf(x, mu, sigma)
 
 
+
+
+
+# Metropolis code for a specific gaussian trial function. Beside the sampled
+# points, it outputs as well the acceptance ratio.
 def Metropolis( x0, delta, n, s):
     
     acc = 0
     points = np.zeros(n, dtype = np.float32)
     points[0] = x0
-    
     x_t = x0
     
     for i in range(1, n):
         x_star = np.random.uniform(x_t - delta, x_t + delta)
-        
-        esp1 = ( -x_star ** 2 / ( 2 * s ** 2) ) # To modify depending on 
-        esp2 = ( -x_t ** 2 / ( 2 * s ** 2) )    # choosen target function.
-                                                # Here the exp are broken to
-        alpha = math.e ** (esp1-esp2)           # avoid "division by 0" issue
-        
+        alpha = min(1, gauss_func(x_star, 0, s) / gauss_func(x_t, 0, s))
         
         if alpha >= np.random.rand() :
             x_t = x_star
@@ -50,6 +58,10 @@ def Metropolis( x0, delta, n, s):
 
 
 
+
+
+
+# Function that prints multiple histograms with gaussian fit and chi square
 def plot_histo(n_arr, func, s, delta):
     
     coefficients = {}
@@ -75,14 +87,12 @@ def plot_histo(n_arr, func, s, delta):
         bin_centers = (bins[:-1] + bins[1:]) / 2
         bin_widths = np.diff(bins)
         density = hist / (n_arr[i] * bin_widths[0])
-
         
         # fit
         params, covariance = curve_fit(gauss_func, bin_centers, density)
         Mu, Sigma = params
         expected_values = gauss_func(bin_centers, Mu, Sigma)
         chi_square = np.sum((density - expected_values) ** 2 / expected_values)
-        
         
         x_range = np.linspace(min(bin_centers), max(bin_centers), 1000)
         
@@ -98,7 +108,6 @@ def plot_histo(n_arr, func, s, delta):
         
         plt.show()
         
-        
         coefficients[n_arr[i]] = (params, covariance, chi_square)
     
     print(coefficients)
@@ -107,7 +116,9 @@ def plot_histo(n_arr, func, s, delta):
         
         
 
-    
+ 
+
+# Gives the acceptance ratio for different values of delta (gaussian trial function)   
 def acc_ratio(x0, n, s, d_arr):
     
     acc = np.zeros(len(d_arr), dtype = np.float32)
@@ -121,6 +132,9 @@ def acc_ratio(x0, n, s, d_arr):
 
 
 
+
+# It allows to estimate the equilibration length looking at the absolute difference
+# between (instantaneous) numerical variance and expected variance
 def equil(x0, delta, n, s, N, N_aver):
     
     l = int(n/N)
@@ -143,6 +157,11 @@ def equil(x0, delta, n, s, N, N_aver):
 
 
 
+# -----------------------------------------------------------------------------
+# SAMPLING PHYSICAL QUANTITIES: DIRECT SAMPLING AND METROPOLIS SAMPLING
+# -----------------------------------------------------------------------------
+
+# Typical Box-Muller code for gaussian sampling
 @njit
 def boxmuller(fagioli):
     
@@ -173,6 +192,10 @@ def boxmuller(fagioli):
 
 
 
+
+
+# Evaluates integrals corresponding to expectation values through a direct
+# sample mean algorithm
 @njit
 def dir_sampl_ground_state(n, s):
     
@@ -196,6 +219,9 @@ def dir_sampl_ground_state(n, s):
 
 
 
+
+# Evaluates integrals corresponding to expectation values through importance
+# sampling, where the points are generated with Metropolis algorithm
 def Metro_sampl_ground_state(n, s):
     
     x0 = 0
@@ -215,6 +241,9 @@ def Metro_sampl_ground_state(n, s):
 
 
 
+
+
+# Gives the absoulte difference between numerical value and analythical solution
 def accuracy(s, lst_n, fun):
     
     E_pot_expected = s**2 / 2
@@ -241,6 +270,13 @@ def accuracy(s, lst_n, fun):
 
 
 
+
+
+# -----------------------------------------------------------------------------
+# CORRELATIONS
+# -----------------------------------------------------------------------------
+
+# Calculates correlations over the sampled points
 @njit
 def corr(n, N_max, lst):
     
@@ -266,6 +302,16 @@ def corr(n, N_max, lst):
     return corr_arr
 
 
+
+
+
+# -----------------------------------------------------------------------------
+# VERIFICATION OF THE BOLTZMANN DISTRIBUTION
+# -----------------------------------------------------------------------------
+
+
+
+# Metropolis code for a Boltzmann trial function (single particle 1D)
 @njit
 def Metropolis_Boltzmann( v0, dvmax, n, kb, T, m):
     
@@ -306,7 +352,10 @@ def Metropolis_Boltzmann( v0, dvmax, n, kb, T, m):
 
 
 
-def Metro_sampl_Boltzmann(v0, dvmax, n, kb, T, m, s): #See pag 59 of "Metropolis.pdf"
+
+# Evaluates expectation values of typical physical quantities through importance
+# sampling + Metropolis (See pag 59 of "Metropolis.pdf")
+def Metro_sampl_Boltzmann(v0, dvmax, n, kb, T, m, s): 
     
     sesamo = Metropolis_Boltzmann(v0, dvmax, n, kb, T, m)[0]
     v_m = np.zeros(n, dtype = np.float32)
@@ -326,6 +375,9 @@ def Metro_sampl_Boltzmann(v0, dvmax, n, kb, T, m, s): #See pag 59 of "Metropolis
 
 
 
+
+
+# stdv of sampled points with block-averages
 @njit
 def block_average(lst, s):
     
@@ -333,7 +385,7 @@ def block_average(lst, s):
     block_size = int(len(lst) / s)
 
     for k in range(s):
-        aver[k] = np.std(lst[(k * block_size):((k + 1) * block_size)])
+        aver[k] = np.mean(lst[(k * block_size):((k + 1) * block_size)])
         
     Sigma_s = np.std(aver)
         
@@ -341,6 +393,9 @@ def block_average(lst, s):
 
 
 
+
+
+# Metropolis code for a Boltzmann trial function (1D gas of N non-interacting particles)
 @njit
 def Metropolis_Boltzmann_N( v0, dvmax, n, kb, T, m, N):
     
@@ -374,7 +429,11 @@ def Metropolis_Boltzmann_N( v0, dvmax, n, kb, T, m, N):
 
 
 
-def Metro_sampl_Boltzmann_N(v0, dvmax, n, kb, T, m, N, s): #See pag 59 of "Metropolis.pdf"
+
+
+# Evaluates expectation values of typical physical quantities through importance
+# sampling + Metropolis (See pag 59 of "Metropolis.pdf")
+def Metro_sampl_Boltzmann_N(v0, dvmax, n, kb, T, m, N, s): 
     
     sesamo = Metropolis_Boltzmann_N(v0, dvmax, n, kb, T, m, N)[0].flatten()
     v_m = np.zeros((n*N), dtype = np.float32)
