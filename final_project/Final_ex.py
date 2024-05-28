67f0f4f00dc02f806970a5ae6f8fab6c8ec44960
Davide Bernocco <davide.plantageneto@gmail.com>
2024-05-28 13:15:28 +0200

    Add files via upload

--- final_project/Final_ex.py
+++ final_project/Final_ex.py
@@ -4,3 +4,65 @@ Plots and other numerical estimations (final project)
 @author: david
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.rcParams['text.usetex'] = True
+
+from FuncF import iteration_tent, bifurcation
+
+
+
+        
+
+Neq = 10
+Niter = 50
+traiett = iteration_tent(0.5, Neq, Niter)
+Ntot = np.arange(Neq + Niter)
+
+fig_traj, ax_traj = plt.subplots(figsize=(6.2, 4.5))
+ax_traj.scatter(Ntot, traiett, marker='o', s=50)
+ax_traj.set_xlabel(r'$ i $', fontsize=15)
+ax_traj.set_ylabel(r'$ x_i $', fontsize=15)
+ax_traj.grid(True)
+plt.show()
+
+
+
+
+
+
+
+
+Neq = 200
+Niter = 50
+r_arr = np.arange(0.2, 2.005, 0.005, dtype = np.float32)
+bif_data = bifurcation(r_arr,Neq, Niter)
+
+
+r_repeated = np.repeat(r_arr, Niter)
+bif_data_flat = bif_data.flatten()
+
+fig_bif, ax_bif = plt.subplots(figsize=(6.2, 4.5))
+ax_bif.scatter(r_repeated, bif_data_flat, marker='o', s=1)
+ax_bif.set_xlabel(r'$ r $', fontsize=15)
+ax_bif.set_ylabel(r'$ x_i $', fontsize=15)
+ax_bif.grid(True)
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- final_project/FuncF.py
+++ final_project/FuncF.py
@@ -4,3 +4,64 @@ Library of self-made functions needed for the final project exercises
 @author: david
 """
 
+import numpy as np
+from numba import njit
+
+
+@njit
+def tent_map(x, r):
+    
+    if x < 1/2:
+        x = r*x
+        
+    else:
+        x = r - r*x
+    
+    return x
+
+
+
+@njit
+def iteration_tent(r, n0, n):
+    trajectory =  np.zeros((n0 + n), dtype = np.float32)
+    x0 = np.random.rand()
+    trajectory[0] = x0
+    x = x0
+    
+    for i in range(1, n0):
+        x = tent_map(x, r)
+        trajectory[i] = x
+        
+    for i in range(n):
+        x = tent_map(x, r)
+        trajectory[i + n0] = x 
+        
+    return trajectory
+
+
+
+
+@njit
+def bifurcation(r, n0, n):
+    accum = np.zeros((len(r), n), dtype = np.float32)
+    
+    
+    for i in range(len(r)):
+        
+        x = np.float32(0.5)
+        
+        for j in range(1, n0):
+            x = np.float32(tent_map(x, r[i]))
+
+        for k in range(n):
+            x = np.float32(tent_map(x, r[i]))
+            accum[i][k] = x
+            
+    return accum
+
+
+
+
+
+
+
